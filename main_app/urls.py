ee7a5f691ccd53bdda3f22a537c450ff48b8939f
Eunice Yang <euniceyang21@gmail.com>
2023-06-04 12:28:55 -0500

    Add routes and views for update and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('parks/', views.park_index, name='park-index'),
   path('parks/<int:park_id>/', views.park_detail, name='park-detail'),
-  path('parks/create/', views.ParkCreate.as_view(), name='park-create')
+  path('parks/create/', views.ParkCreate.as_view(), name='park-create'),
+  path('parks/<int:pk>/update/', views.ParkUpdate.as_view(), name='park-update'),
+  path('parks/<int:pk>/delete/', views.ParkDelete.as_view(), name='park-delete'),
 ]
