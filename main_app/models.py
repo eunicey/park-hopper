90b276326ea5c29dfdc1164fc889ab3d5d2dba02
Eunice Yang <euniceyang21@gmail.com>
2023-06-04 13:27:04 -0500

    Update park model and routes to include authentication

--- main_app/models.py
+++ main_app/models.py
@@ -14,7 +14,8 @@ class Park(models.Model):
   state = models.CharField('State (abbreviation)', max_length = 2)
   year_visited = models.IntegerField('If visited, what year', blank = True, null = True)
   highlights = models.TextField(max_length = 250, blank = True, null = True)
-
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
   def __str__(self):
     return self.name
   
