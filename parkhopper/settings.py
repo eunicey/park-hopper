a6f89af7bdecddb2b91aaaa2692b375045a92edf
Eunice Yang <euniceyang21@gmail.com>
2023-06-04 13:57:40 -0500

    Update nav bar dynamically based on login status

--- parkhopper/settings.py
+++ parkhopper/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = 'park-index'
 
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
