9e01d94dd26f2366cdca1cc05c61393eab294aef
Eunice Yang <euniceyang21@gmail.com>
2023-06-04 13:34:47 -0500

    Update home template to include login functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('parks/', views.park_index, name='park-index'),
-  
+
   path('parks/<int:park_id>/', views.park_detail, name='park-detail'),
   path('parks/<int:park_id>/add-activity/', views.add_activity, name='add-activity'),
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
 from .models import Park
 from .forms import ActivityForm
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
