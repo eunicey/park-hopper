eb5bcb5f5292bb171c809efc658a61ec9b662c2e
Eunice Yang <euniceyang21@gmail.com>
2023-06-04 13:02:02 -0500

    Add activity form to parks detail

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .models import Park
+from .forms import ActivityForm
 
 def home(request):
   return render(request, 'home.html')
@@ -15,7 +16,11 @@ def park_index(request):
 
 def park_detail(request, park_id):
   park = Park.objects.get(id=park_id)
-  return render(request, 'parks/detail.html', {'park': park})
+  activity_form = ActivityForm()
+  return render(request, 'parks/detail.html', {
+    'park': park,
+    'activity_form': activity_form,
+  })
 
 class ParkCreate(CreateView):
   model = Park
