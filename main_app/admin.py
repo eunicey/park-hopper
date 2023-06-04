bfd54cbf9c7844488689a263108a1b057fae1548
Eunice Yang <euniceyang21@gmail.com>
2023-06-04 15:30:17 -0500

    Update templates to include form for uploading photos and to display user photos

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Park, Activity
+from .models import Park, Activity, Photo
 
 admin.site.register(Park)
 admin.site.register(Activity)
+admin.site.register(Photo)
